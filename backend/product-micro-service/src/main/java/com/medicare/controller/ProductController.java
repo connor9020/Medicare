bf3b6c3eddad4b82b67a89c6fdd32e5cc82aabba
Connor Sandry <connor9020@gmail.com>
2024-07-17 01:55:12 -0400

    adding product microservice

---
+++ backend/product-micro-service/src/main/java/com/medicare/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.medicare.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.medicare.entity.Product;
+import com.medicare.service.ProductService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
+
